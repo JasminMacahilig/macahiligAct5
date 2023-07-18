eda1532706b4191c63c478874ce438a8d60a06df
JasminMacahilig <139834312+JasminMacahilig@users.noreply.github.com>
2023-07-18 11:10:33 +0800

    Add files via upload

---
+++ activity5/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Data
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myArray(,) As Object = {
+{"Jasmine", 20, "Female"},
+{"Jocelyn", 20, "Female"},
+{"Jessa", 20, "Female"},
+{"Mitch", 21, "Female"},
+{"Abet", 23, "Male"},
+{"Darwin", 26, "Male"},
+{"Angelo", 22, "Male"},
+
+}
+
+        DisplayArrayInDataGridView(myArray)
+    End Sub
+
+    Private Function ConvertArrayToDataTable(ByVal dataArray As Object(,)) As DataTable
+        Dim dataTable As New DataTable()
+
+        ' Set the column headers
+        dataTable.Columns.Add("Name")
+        dataTable.Columns.Add("Age")
+        dataTable.Columns.Add("Gender")
+
+        ' ma add raun kita it Rows sa Data Table
+        Dim rowsCount As Integer = dataArray.GetLength(0)
+        Dim columnsCount As Integer = dataArray.GetLength(1)
+
+        For i As Integer = 0 To rowsCount - 1
+            Dim rowValues(columnsCount - 1) As Object
+            For j As Integer = 0 To columnsCount - 1
+                rowValues(j) = dataArray(i, j)
+            Next
+            dataTable.Rows.Add(rowValues)
+        Next
+
+        Return dataTable
+    End Function
+
+    Private Sub DisplayArrayInDataGridView(ByVal dataArray As Object(,))
+
+        Dim dataTable As DataTable = ConvertArrayToDataTable(dataArray)
+
+        DataGridView1.DataSource = dataTable
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
